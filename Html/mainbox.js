b193913a3063e96cc9987fb15360330dd4fa74c8
kkys00 <y_seo00@naver.com>
2021-08-19 23:32:56 +0900

    delete to-do list tag

--- Html/mainbox.js
+++ Html/mainbox.js
@@ -1,7 +1,13 @@
 const searchBtn = document.querySelector('#search');
 const searchForm = document.querySelector('#search-form');
-
+let fadespeed = 300;
+const $todoContainer = $('#main-container');
 
 searchBtn.addEventListener('click', ()=>{
     searchForm.classList.toggle('active');
 })
+
+function deleteP(child){
+    let parent = child.parentNode;
+    parent.parentNode.removeChild(parent);
+}

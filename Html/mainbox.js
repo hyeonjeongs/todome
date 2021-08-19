8f7aae900b85e585951a22a4a671121a81dc9d37
kkys00 <y_seo00@naver.com>
2021-08-19 23:45:28 +0900

    fix searchform

--- Html/mainbox.js
+++ Html/mainbox.js
@@ -1,11 +1,10 @@
 const searchBtn = document.querySelector('#search');
 const searchForm = document.querySelector('#search-form');
 let fadespeed = 300;
-const $todoContainer = $('#main-container');
 
 searchBtn.addEventListener('click', ()=>{
     searchForm.classList.toggle('active');
-})
+});
 
 function deleteP(child){
     let parent = child.parentNode;

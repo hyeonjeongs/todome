0268e299655578c34ebda30eb864e421fcb6f404
kkys00 <y_seo00@naver.com>
2021-08-20 15:12:18 +0900

    add button

--- Html/mainbox.js
+++ Html/mainbox.js
@@ -1,6 +1,7 @@
 const searchBtn = document.querySelector('#search');
 const searchForm = document.querySelector('#search-form');
 let fadespeed = 300;
+const addTodoForm = document.querySelector('.add-container')
 
 searchBtn.addEventListener('click', ()=>{
     searchForm.classList.toggle('active');
@@ -9,4 +10,8 @@ searchBtn.addEventListener('click', ()=>{
 function deleteP(child){
     let parent = child.parentNode;
     parent.parentNode.removeChild(parent);
+}
+
+function showAddTodoForm(){
+    addTodoForm.classList.toggle('active');
 }

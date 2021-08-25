5fd07dc7322a4c13c0a218c641b02d1fd7ba0c12
Yeongseo Kim <88617509+kkys00@users.noreply.github.com>
2021-08-25 23:56:50 +0900

    todoinput

---
+++ code/todoinput.js
@@ -0,0 +1,65 @@
+const inputBox = document.querySelector(".input-text-box");
+const input = inputBox.querySelector("input");
+const toDoBox = document.querySelector("#todo-box1");
+const toDoList = toDoBox.querySelector(".list");
+const toDoText = toDoList.querySelector("span");
+
+const allToDoList = document.querySelectorAll(".list");
+
+const TODO = "toDoThing"
+
+function deleteP(child){
+    let parent = child.parentNode;
+    parent.parentNode.removeChild(parent);
+    showNothing();
+}
+
+function addToDoText(text){
+    const li = document.createElement("li");
+    li.className = "items";
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "CHECK";
+    checkbox.value = 0;
+    const span = document.createElement("span");
+    span.innerText = ` ${text} `;
+    const delBtn = document.createElement("button");
+    delBtn.className = "btn-delete";
+    delBtn.onclick = function(){
+        deleteP(delBtn);
+    }
+    const i = document.createElement("i");
+    i.className = "fas fa-times-circle";
+    delBtn.appendChild(i);
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    showNothing();
+}
+
+function saveToDo(text){
+    localStorage.setItem(keyid, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentInput = input.value;
+    addToDoText(currentInput);
+}
+function showNothing(){
+    for(let index = 0; index<allToDoList.length; index++){
+        if(allToDoList[index].childElementCount === 0){
+            allToDoList[index].nextElementSibling.classList.add('list-active');
+        } else{
+            allToDoList[index].nextElementSibling.classList.remove('list-active');
+        }
+    }
+}
+
+function init(){
+    inputBox.addEventListener("submit", handleSubmit);
+    showNothing();
+}
+
+init()
